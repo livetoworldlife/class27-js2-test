d1c00355f0d5acf18c0c9091d2f197875d47b27b
livetowordlife <livetoworldlife@gmail.com>
2020-05-07 11:23:02 +0200

    test was finished by Ersin

--- js2-test.js
+++ js2-test.js
@@ -13,6 +13,21 @@
     Expected output:
     'Noer is 28 and works as an Education Director'
 */
+/*
+function logObj(obj) {
+  let name = obj.name;
+  let age = obj.age;
+  let job = obj.job;
+  return `${name} is ${age} and works as an ${job}!`;
+
+}
+const newObj = {
+  name: "Ersin",
+  age: 33,
+  job: "web developer"
+};
+console.log(logObj(newObj));
+*/
 
 /* 2. Create a function, named logNumbers, that:
       - Takes 2 arguments: a start number and an end number
@@ -39,12 +54,32 @@
       Use the following values: 1 (start number) and 100 (end number)
 */
 
+/*
+function logNumbers(startN, endN) {
+  for (let i = startN; i <= endN; i++) {
+    if (i % 15 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    }
+
+    console.log(i);
+
+  }
+}
+logNumbers(1, 15);
+
+*/
+
+
 // JavaScript2
 
 /* 3. Create a function, named toNumbers, that:
       - Takes in 1 argument: an array of strings
       - Get the index position number of each value
-      - Create a new array that includes all these numbers 
+      - Create a new array that includes all these numbers
       - Make use of the map() function
       - Log to the console the array of numbers
       - Return the array of numbers
@@ -54,6 +89,17 @@
 
       Use the following array: const letters = ['a', 'b', 'c', 'd'];
 */
+/*
+function toNumbers(arr) {
+  const indexArr = arr.map((value, index, array) => {
+    return index + 1;
+  });
+  return indexArr;
+}
+const letters = ['a', 'b', 'c', 'd'];
+
+console.log(toNumbers(letters));
+*/
 
 /* 4. Create a function, called injectBooksToDOM, that:
       - Takes 1 argument: an array of objects
@@ -61,9 +107,9 @@
       - Injects each object's content into the DOM in an unordered list
       - Make use of the forEach() function
       - Returns true if it works, false if it doesn't
-  
+
       Target the '#root' element in 'index.html'.
-      Use the following object: 
+      Use the following object:
       const books = [
             {
                   bookName: "The Nature of Software Development",
@@ -79,28 +125,68 @@
             }
       ];
 */
+function injectBooksToDOM(booksArr) {
+  const root = document.getElementById('root');
+  let ul = document.createElement('ul');
+  root.appendChild(ul);
+  booksArr.forEach((element, index, arr) => {
+    console.log(arr)
+    let li = document.createElement('li');
+    let p = document.createElement("p");
+    p.innerHTML = `book name ; ${arr[index].bookName} and author ; ${arr[index].author}
+    <img src="${arr[index].coverURL}">`;
+    ul.appendChild(li);
+    li.appendChild(p);
+  });
 
+}
+const books = [
+  {
+    bookName: "The Nature of Software Development",
+    author: "Ron Jeffries",
+    coverURL:
+      "https://cdn-images-1.medium.com/max/1200/1*CQRh-pFTZ97ReXogbefleQ.png"
+  },
+  {
+    bookName: "Clean Code",
+    author: "Robert Cecil Martin",
+    coverURL:
+      "https://images-na.ssl-images-amazon.com/images/I/515iEcDr1GL._SX258_BO1,204,203,200_.jpg"
+  }
+];
+window.onload = injectBooksToDOM(books);
 /*
 5. What's the output for this snippet? Is the output deterministic (always the same) or might it depend on the browser
-we run this in? 
-Explain your answer in less than 150 words. (The 'yes or no' answer doesn't really matter to us, 
+we run this in?
+Explain your answer in less than 150 words. (The 'yes or no' answer doesn't really matter to us,
 the explanation why is the important part)
 
 function test() {
-    console.log('one');
-    setTimeout(function() {
-        console.log('two');
-        setTimeout(function() {
-            console.log('three');
-        }, 0);
-    }, 1000);
-    setTimeout(function() {
-        console.log('four');
-        setTimeout(function() {
-            console.log('five');
-        }, 1000);
+  console.log('one');
+  setTimeout(function () {
+    console.log('two');
+    setTimeout(function () {
+      console.log('three');
     }, 0);
-    console.log('six');
+  }, 1000);
+  setTimeout(function () {
+    console.log('four');
+    setTimeout(function () {
+      console.log('five');
+    }, 1000);
+  }, 0);
+  console.log('six');
 }
 test(); // ?
+*/
+
+/*
+we defined test function.
+first 'one' is come the call stack then is log in console.
+then there are two callback functions-setTimeout they will throw the callback API then call queue then event loop. after call stack is empty, their turns will start
+second 'six' is come the call stack then is log in console.
+third 'four' because setTimeout function has '0' for timeout. there is a new call back function into that. it will come later callback API
+fourth 'two' because setTimeout function has '1000' for timeout. there is a new call back function into that. it will come later callback API
+fifth 'three' because inner setTimeout function has '0' for timeout.
+sixth 'five' is log in console.because inner setTimeout function has '1000' for timeout
 */
